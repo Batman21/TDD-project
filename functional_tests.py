134c34acb119871173ee402c551626cd3e883025
Jairo Batista <jairo.batista21@gmail.com>
2014-10-02 19:04:10 -0400

    Functional test update.

--- functional_tests.py
+++ functional_tests.py
@@ -16,6 +16,26 @@ class NewVisitorTest(unittest.TestCase):
 
         # User notices title tab
         self.assertIn('To-do', self.browser.title)
+
+        # User is invited to enter a to-do item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # User types "Economics"
+        inputbox.send_keys('Economics')
+
+        # User hits enter, page updates with item
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'Economics' for row in rows)
+        )
+
         self.fail('Finish test!')
 
 
