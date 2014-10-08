b233818c39853824f7d7233f9cf2580b5c66734e
Jairo Batista <jairo.batista21@gmail.com>
2014-10-08 15:22:11 -0400

    Functional test update.

--- functional_tests.py
+++ functional_tests.py
@@ -12,33 +12,52 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # X has heard about a cool new online to-do app. X goes
+        # to check out its homepage
         self.browser.get('http://localhost:8000')
 
-        # User notices title tab
+        # X notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # User is invited to enter a to-do item.
+        # X is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-        # User types "Economics"
-        inputbox.send_keys('Economics')
+        # X puts Economics Assignment as her first task.
+        inputbox.send_keys('Economics Assignment')
 
-        # User hits enter, page updates with item
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Economics Assignment" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Economics Assignment')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Metaphysics Assignment."
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Metaphysics Assignment')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == 'Economics' for row in rows),
-            'New to-do item does not appear in table.'
-        )
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Metaphysics Assignment')
+        self.check_for_row_in_list_table('1: Economics Assignment')
 
-        self.fail('Finish test!')
+        # X wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+        self.fail('Finish the test!')
+
+        # She visits that URL - her to-do list is still there.
 
 
 if __name__ == '__main__':
